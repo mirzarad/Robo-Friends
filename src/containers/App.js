d716d2adf56136c3f9ceca2c90bf9c3f2f447f00
Mirza Radoncic <mirza.radoncic@stonybrook.edu>
2020-05-05 22:48:55 -0400

    Removed ErrorBoundary: unecessary for this projects purposes

--- src/containers/App.js
+++ src/containers/App.js
@@ -36,9 +36,7 @@ class App extends Component{
                     <h1 className='f1'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
                     <Scroll>
-                        <ErrorBoundary>
                             <CardList robots={filteredRobots}/>
-                        </ErrorBoundary>
                     </Scroll>
                 </div>
             )
