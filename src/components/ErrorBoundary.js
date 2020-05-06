62b7cc2bcb16eabfe44148672ea0b029033dba5b
Mirza Radoncic <mirza.radoncic@stonybrook.edu>
2020-05-05 22:11:29 -0400

    Added ErrorBoundaries to project

---
+++ src/components/ErrorBoundary.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class ErrorBoundary extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            hasError: false
+        }
+    }
+
+    componentDidCatch(error, info){
+        this.setState({ hasError: true }) 
+    }
+
+    render(){
+        if(this.state.hasError){
+        
+        }
+    }
+}
+
+export default ErrorBoundary
