23d667c3da1f81b56b78f2dd941749105e31ea75
Mirza Radoncic <mirza.radoncic@stonybrook.edu>
2020-05-09 01:34:26 -0400

    Implemented redux for state change overhaul to project

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 import App from './containers/App'
 import './index.css';
 import * as serviceWorker from './serviceWorker';
+import { searchRobots } from './reducers'
 import 'tachyons';
 
+const store = createStore(searchRobots)
 
 ReactDOM.render(
-  <div>
-    <App/>
-  </div>,
+  <App store = {store}/>,
   document.getElementById('root')
 );
 
