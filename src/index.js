0ecda2ed7055f966cf5174b103d21fd1e1d36bcf
Mirza Radoncic <mirza.radoncic@stonybrook.edu>
2020-05-10 22:26:37 -0400

    Added thunk middleware to project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, combineReducers } from 'redux'
+import { createLogger } from 'redux-logger'
+import thunkMiddleWare from 'redux-thunk'
 import App from './containers/App'
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import { searchRobots } from './reducers'
+import { searchRobots, requestRobots } from './reducers'
 import 'tachyons';
 
-const store = createStore(searchRobots)
+const logger = createLogger()
+
+const rootReducer = combineReducers({searchRobots, requestRobots})
+const store = 
+  createStore(rootReducer, applyMiddleware(thunkMiddleWare, logger))
 
 ReactDOM.render(
   <App store = {store}/>,
